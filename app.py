8e0b7e1dee1a35c142805b73f3d28176c875e07f
Maikon Silva <55884605+m4ik-crtl@users.noreply.github.com>
2025-04-26 00:18:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.header('Jogando uma moeda')
 
 st.write('Ainda não é um aplicativo funcional. Em construção.')
+
+
+$ ls -a
+.git .gitignore .streamlit app.py README.md requirements.txt
